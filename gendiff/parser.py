da4fa49b9e6e78cdd226c06ee53519110635f663
octodes <octode@gmail.com>
2025-02-19 15:07:41 +0300

    add siple yaml and tests

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,5 +1,17 @@
 import json
+from pathlib import Path
+
+import yaml
 
 
 def parse(file_path):
-    return json.load(open(file_path))
+    file_extension = Path(file_path).suffix
+
+    match file_extension:
+        case ".json":
+            return json.load(open(file_path))
+        case ".yml" | ".yaml":
+            return yaml.safe_load(open(file_path))
+        case _:
+            raise ValueError(f"Unknown file type: {file_extension[1:]}")
+
